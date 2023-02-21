196cf57c713fe163988b0ba71107befa856347e4
Oleksandr Kokhno <kohnoan@gmail.com>
2023-02-21 21:11:43 +0200

    finish2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulRef = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+
+const itemRef = document.createElement('li');
+  itemRef.textContent = ingredient;
+  itemRef.classList.add('item');
+  console.log(itemRef);
+  ulRef.append(itemRef);
+
+  return itemRef;
+
+});
+
+console.log(elements);
+
+// ulRef.append(itemRef);
+
