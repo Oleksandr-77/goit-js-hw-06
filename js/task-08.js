f62f59dbf9175daa715576e9333908f578fd6058
Oleksandr Kokhno <kohnoan@gmail.com>
2023-02-25 22:59:36 +0200

    finish09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const formInputEl = document.querySelector('.login-form');
+
+formInputEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(event);
+
+    const {
+        elements: {email, password},
+    } = event.currentTarget;
+
+    if (email.value ==='' || password.value === '') {
+        return alert('Всі поля повинні бути заповнені!');
+    }
+    const formElements = {email: email.value, password: password.value};
+
+    console.log(formElements);
+
+    event.currentTarget.reset;
+}
+
 
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonColorEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+
+function changeButtonColor() {
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  console.log(colorEl.textContent = color);
+  
+}
+
+buttonColorEl.addEventListener('click', changeButtonColor);
