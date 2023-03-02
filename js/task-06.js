a1a303d3ae00192a41e380b40bcfd2d3d33e20e8
Oleksandr Kokhno <kohnoan@gmail.com>
2023-03-02 22:49:21 +0200

    final

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@
 const inputEl = document.querySelector("#validation-input");
 
 inputEl.addEventListener("blur", () => {
-    const result = inputEl.value.length === Number(inputEl.getAttribute("data-length"));
+    const result = inputEl.value.trim().length === Number(inputEl.getAttribute("data-length"));
 
     console.log(result);
     console.log(inputEl.classList.toggle("valid", result));
